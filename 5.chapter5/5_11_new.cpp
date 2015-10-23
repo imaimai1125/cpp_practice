5824a608cb260530e61c5ea6834a97bb8e1609ed
imaimai1125 <imaimai1125@gmail.com>
2015-10-23 20:51:07 +0900

    10/23 ５章の終わりまで

--- 5.chapter5/5_11_new.cpp
+++ 5.chapter5/5_11_new.cpp
@@ -1,1 +1,45 @@
-	
+#include <iostream>
+using namespace std;
+
+
+void CalcMultiples(int* array, int size, int n){
+	for (int i = 0; i < size; ++i){
+		array[i] = n * (i+1);
+	}
+}
+void ShowArray(const int* array, int size){
+	for (int i = 0; i < size; ++i)
+	{
+		cout << array[i] << " ";
+	}
+	cout << endl;
+}
+int main(int argc, char const *argv[])
+{
+	//nothrowを入れておくと、配列が確保できなかった時にnull pointerを返す
+	int* p = new(nothrow) int;
+	//pに値を代入している。謎だ。
+	if(p!=NULL){
+		*p = 1;
+		cout << *p << endl;
+		delete p;
+	}
+	int size = 3;
+	int* array;
+	cout << "How far do you caluculate ?? -- " << flush;
+	cin >> size;
+
+	array = new int[size];
+	CalcMultiples(array,size,3);
+	ShowArray(array,size);
+
+
+	//これはOKなのか？newで配列を確保してやると、配列のサイズを可変にすることができる。
+	cout << "How far do you caluculate ?? -- " << flush;
+	cin >> size;
+	// array = new int[size];
+	CalcMultiples(array,size,3);
+	ShowArray(array,size);
+
+	delete[] array;
+}
