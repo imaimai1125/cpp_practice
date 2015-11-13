9ccb5084679b91aea7cb237fcdc3f61ac49e5503
imaimai <imaimai1125@gmail.com>
2015-11-14 00:48:43 +0900

    11/13 14.1-14.2 -p728

---
+++ 14.chapter14/14_1_ShortCircuit.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+bool Input(double& value){
+	cout << "Input the number." << endl;
+	value = 0;
+	cin >> value;
+	return value != 0;
+}
+
+int main(){
+	double a,b;
+	while(Input(a) && Input(b)){
+		cout << "a/b = " << (a/b) << endl
+			 << "b/a = " << (b/a) << endl;
+	}
+}
